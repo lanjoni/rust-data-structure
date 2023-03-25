f30f1a3a49227d6a59704ded7facb969a51f82a2
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2023-03-25 20:30:29 -0300

    update: removing targets on binary-tree/basic-insert

--- binary-tree/basic-insert/target/debug/deps/binary_tree-1ac9e7cb3e348729.d
+++
@@ -1,7 +0,0 @@
-/home/guto/Programming/rust/binary-tree/target/debug/deps/binary_tree-1ac9e7cb3e348729.rmeta: src/main.rs src/classes/mod.rs src/classes/tree.rs
-
-/home/guto/Programming/rust/binary-tree/target/debug/deps/binary_tree-1ac9e7cb3e348729.d: src/main.rs src/classes/mod.rs src/classes/tree.rs
-
-src/main.rs:
-src/classes/mod.rs:
-src/classes/tree.rs:
--- binary-tree/basic-insert/target/debug/deps/binary_tree-fce6a1defa05d45f.d
+++
@@ -1,7 +0,0 @@
-/home/guto/Programming/rust/binary-tree/target/debug/deps/binary_tree-fce6a1defa05d45f: src/main.rs src/classes/mod.rs src/classes/tree.rs
-
-/home/guto/Programming/rust/binary-tree/target/debug/deps/binary_tree-fce6a1defa05d45f.d: src/main.rs src/classes/mod.rs src/classes/tree.rs
-
-src/main.rs:
-src/classes/mod.rs:
-src/classes/tree.rs:
